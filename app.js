cec63eaa9f008d50ddeef8a4b6273880fb8cf679
Rohitt Vashishtha <aero31aero@gmail.com>
2016-07-01 14:50:44 +0530

    Added skeleton code to app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+var forum = function () {
+    var profileIntegrator = function (identifier) {
+        return identifier;
+    };
 
+    function setProfileIntegrator(callback) {
+        profileIntegrator = callback;
+    }
+    
+    function getExpressRouter(){
+        
+    }
+    
+    return {
+        setProfileIntegrator: setProfileIntegrator,
+        getExpressRouter: getExpressRouter;
+    }
+}();
+
+module.exports = forum;
